a20b5a9b968cf99a4f6057a65ce52abdbf0d8241
Carlos Manotas <carlosmanotas@gmail.com>
2021-02-06 23:38:49 +0100

    change swapi api
    the http://swapi.io is down, and now others devs have this new site https://swapi.dev/

--- src/app/servicios.service.ts
+++ src/app/servicios.service.ts
@@ -8,8 +8,8 @@ export class ServiciosService {
 
   constructor(private http: Http) { }
 
-  private listaUrl = "http://swapi.co/api/people/";
-  private detailUrl = "http://swapi.co/api/people/?search=";
+  private listaUrl = "https://swapi.dev/api/people/";
+  private detailUrl = "https://swapi.dev/api/people/?search=";
 
   getList (){
     return this.http.get(this.listaUrl)
